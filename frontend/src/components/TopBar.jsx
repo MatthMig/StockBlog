9347fe24994d83630870b66b4d2533e55a80d785
Matthieu MIGNE <matthieu.migne@orange.fr>
2024-04-15 09:43:22 +0200

    Top Bar implementation

---
+++ frontend/src/components/TopBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import '../css/TopBar.css';
+import ConnectionSettingsButton from './ConnectionSettingsButton';
+
+const TopBar = ({ isLoggedIn, onLogin, onSignUp, onUserSettings }) => {
+    return (
+        <Navbar bg="light" expand="lg" variant="light" className="scrolled">
+            <Navbar.Brand href="#home" className="logo">StockBlog</Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <ConnectionSettingsButton 
+                        isLoggedIn={isLoggedIn}
+                        onLogin={onLogin}
+                        onSignUp={onSignUp}
+                        onUserSettings={onUserSettings}
+                    />
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default TopBar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import TopBar from "../components/TopBar";
 
 function HomePage() {
     return (
-        <>
-            
-        </>
+        <body>
+            <header>
+                <TopBar />
+            </header>
+            <main>
+                <h1>Welcome to StockBlog</h1>
+            </main>
+        </body>
     );
 }
 
