c73095864309752c5b3d678d8db8529b2ce28460
Gabriel SORIA RAMOS <gabriel.soria-ramos@grenoble-inp.fr>
2024-04-15 11:10:30 +0200

    dependencies resolved

--- frontend/src/pages/NoStock.jsx
+++ frontend/src/pages/NoStock.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Dashboard from "../components/dashboard";
 
 function NoStock() {
     return (
       <div className="AppContainer">
         <textarea name="" id="" cols="30" rows="10">lololoo</textarea>
-        <Dashboard />
+        {/* <Dashboard /> */}
       </div>
     );
 }
